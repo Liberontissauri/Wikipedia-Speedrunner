c3e64b0493634c9ffa4fb2ec72be73a3ac005792
Liberontissauri <vaz.liberato@gmail.com>
2020-04-22 19:43:06 +0100

    V 0.1
    wikipedia.py added:
    function getlinks() added
    function mergelinks() added

---
+++ wikipedia.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getlinks(URL):
+    page = requests.get(URL)
+
+    links = []
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    for paragraph in soup.findAll("p"):
+        if None in (paragraph):
+            continue
+        for tag in paragraph.findAll("a"):
+            if tag.get("href")[0] == "/" and "/wiki/Wikipedia:" not in tag.get("href") and "/w/index.php" not in tag.get("href"):
+                links.append(tag.get("href"))
+
+    return links
+
+print(getlinks("https://en.wikipedia.org/wiki/Pok%C3%A9mon_Sword_and_Shield"))
+
+def mergelinks(linklists):
+    finallist = []
+    for linklist in linklists:
+            for link in linklist:
+                if link not in finallist:
+                    finallist.append(link)
